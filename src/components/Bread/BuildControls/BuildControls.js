abc65693da16d28923316143b4a8e418a036e0ca
Abiodun <beycity1@gmail.com>
2022-07-28 14:57:16 +0100

    updates2

--- src/components/Bread/BuildControls/BuildControls.js
+++ src/components/Bread/BuildControls/BuildControls.js
@@ -1,7 +1,33 @@
 import React from "react";
 import classes from '../BuildControls/BuildControls.module.css'
+import BuildControl from "./BuildControl/BuildControl";
+
+const Control = [
+  {label: "cheese", type: "cheese"},
+  {label: "salad", type: "salad"},
+  {label: "meat" , type: "meat"},
+  {label: "bacon", type: "bacon"}
+  
+]
+
 
 const BuildControls = (props) =>{
-
+  return(
+          <div className={classes.BuildControls}>
+            {Control.map(ctrl =>{ 
+              return <BuildControl
+              key = {ctrl.label}
+              label = {ctrl.label}
+              added = {()=>props.ingredientAdded(ctrl.type)}
+              delete ={()=>props.ingredientDelete(ctrl.type)}
+            />
+          })}
+          </div>
+        
+  )
 }
+
+
+
+
 export default BuildControls;
--- src/containers/Burger/Burger.js
+++ src/containers/Burger/Burger.js
@@ -1,26 +1,83 @@
 import React, { Component } from 'react'
 import Aux from '../../hoc/Auxilliary'
 import Bread from '../../components/Bread/Bread'
+import BuildControls from '../../components/Bread/BuildControls/BuildControls';
+
+
+const INGREDIENTS_PRICES ={
+    salad: 50,
+    cheese: 100,
+    bacon: 70,
+    meat: 10,
+}
+
+
+
+
 
 class Burger extends Component{
 
     state = {
         ingredients: {
             salad: 0,
-            cheese: 5,
+            cheese: 0,
             bacon: 0,
             meat: 0,
-        }
+        }, 
+        totalPrice: 500,
     }
+
+addIngredientHandler = (type) =>{
+    const oldCount = this.state.ingredients[type]
+    const updatedCount = oldCount+1
+    const updateIngredients = {
+        ...this.state.ingredients
+    }
+updateIngredients[type] = updatedCount;
+const priceAddition = INGREDIENTS_PRICES[type]
+const oldPrice = this.state.totalPrice
+const newPrice = oldPrice + priceAddition
+this.setState({
+    totalPrice: newPrice,
+    ingredients: updateIngredients
+
+})
+
+}
+
+deleteIngredientHandler = (type) =>{
+    const oldCount = this.state.ingredients[type]
+    const updatedCount = oldCount-1
+    const updateIngredients = {
+        ...this.state.ingredients
+    }
+updateIngredients[type] = updatedCount;
+const priceDeletion = INGREDIENTS_PRICES[type]
+const oldPrice = this.state.totalPrice
+const newPrice = oldPrice - priceDeletion
+this.setState({
+    totalPrice: newPrice,
+    ingredients: updateIngredients
+
+})
+
+}
+
+
+
+
+
 
 render(){
     return(
         <Aux>
             <Bread ingredients = {this.state.ingredients}/>
-             <h2>Control</h2>
-
+            <BuildControls 
+            ingredientAdded = {this.addIngredientHandler}
+            ingredientDelete ={this.deleteIngredientHandler} />
+            
         </Aux>
-
+    
     )
 
 }
